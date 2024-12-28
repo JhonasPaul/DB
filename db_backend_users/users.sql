dbd41621cd87028797a122c510f6a2bfd8d27744
jonathan <johnkord17@gmail.com>
2024-12-27 23:51:45 -0500

    users

--- db_backend_users/users.sql
+++ db_backend_users/users.sql
@@ -1,9 +1,7 @@
 create database db_backend_users;
 use db_backend_users;
-select *
-from users;
-alter table users
-    auto_increment = 1;
+select * from users;
+alter table users auto_increment = 1;
 
 create table users
 (
@@ -15,26 +13,18 @@ create table users
     email    varchar(50)  not null unique
 );
 
-alter table users
-    add passwpod VARCHAR(255) not null;
-alter table users
-    add email VARCHAR(50) not null;
-alter table users
-    add unique (email);
-alter table users
-    add unique (email);
+alter table users add passwpod VARCHAR(255) not null;
+alter table users add email VARCHAR(50) not null;
+alter table users add unique (email);
+alter table users add unique (email);
 
 
-INSERT INTO users (name, lastname, username, password, email)
-VALUES ('Pool', 'Ortiz', 'pool', '123123', 'pool@mail.com');
-INSERT INTO users (name, lastname, username, password, email)
-VALUES ('Josefa', 'Doe', 'admin', '123123', 'josefa@mail.com');
-INSERT INTO users (name, lastname, username, password, email)
-VALUES ('John Juan', 'Doe Roe', 'john', '321321', 'john@email.com');
+INSERT INTO users (name, lastname, username, password, email) VALUES ('Pool', 'Ortiz', 'pool', '123123', 'pool@mail.com');
+INSERT INTO users (name, lastname, username, password, email) VALUES ('Josefa', 'Doe', 'admin', '123123', 'josefa@mail.com');
+INSERT INTO users (name, lastname, username, password, email) VALUES ('John Juan', 'Doe Roe', 'john', '321321', 'john@email.com');
 
 
-select *
-from roles;
+select * from roles;
 
 create table roles
 (
@@ -43,13 +33,10 @@ create table roles
 
 );
 
-insert into roles(name)
-values ('ROLE_USER');
-insert into roles(name)
-values ('ROLE_ADMIN');
+insert into roles(name) values ('ROLE_USER');
+insert into roles(name) values ('ROLE_ADMIN');
 
-select *
-from users_roles;
+select * from users_roles;
 
 create table users_roles
 (
@@ -60,13 +47,8 @@ create table users_roles
     foreign key (user_id) references users (id)
 );
 
-insert into users_roles(user_id, role_id)
-VALUES (1, 1);
-insert into users_roles(user_id, role_id)
-VALUES (2, 1);
-insert into users_roles(user_id, role_id)
-VALUES (2, 2);
-insert into users_roles(user_id, role_id)
-VALUES (3, 2);
-insert into users_roles(user_id, role_id)
-VALUES (3, 1);
+insert into users_roles(user_id, role_id) VALUES (1, 1);
+insert into users_roles(user_id, role_id) VALUES (2, 1);
+insert into users_roles(user_id, role_id) VALUES (2, 2);
+insert into users_roles(user_id, role_id) VALUES (3, 2);
+insert into users_roles(user_id, role_id) VALUES (3, 1);
